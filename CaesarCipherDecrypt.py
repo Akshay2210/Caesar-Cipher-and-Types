a33ae88b6ad628e198bd3bf7f6b30de92f2df265
Akshay Bhalerao <Akshay2210>
2020-07-24 10:51:25 +0530

    Add all my files

---
+++ CaesarCipherDecrypt.py
@@ -0,0 +1,12 @@
+CaesarCipherDecrypt.py
+a=str(input("Enter encrypted text:"))
+key=int(input("Enter key:"))
+decypt=""
+for i in range (len(a)):
+    char=a[i]
+    if(char.isupper()):
+        decypt += chr((ord(char) - key-65) % 26 + 65)
+    else:
+        decypt += chr((ord(char) - key-97) % 26 + 97)
+print("Decrypted text is:")
+print(decypt)
---
+++ CaesarCipherEncrypt.py
@@ -0,0 +1,12 @@
+
+a=str(input("Enter text to encrypt:"))
+key=int(input("Enter key:"))
+enc=""
+for i in range (len(a)):
+    char=a[i]
+    if(char.isupper()):
+        enc += chr((ord(char) + key-65) % 26 + 65)
+    else:
+        enc += chr((ord(char) + key-97) % 26 + 97)
+print("Encrypted Text is:")
+print(enc)
